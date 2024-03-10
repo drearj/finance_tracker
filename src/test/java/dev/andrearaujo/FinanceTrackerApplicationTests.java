ae14b7acfa2255311202152ef065b2e7295d4d19
André Araujo <andreolv.ar@gmail.com>
2024-03-10 12:33:44 -0300

    chore: initial project setup

---
+++ src/test/java/dev/andrearaujo/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrearaujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FinanceTrackerApplicationTests.class)
+class FinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
